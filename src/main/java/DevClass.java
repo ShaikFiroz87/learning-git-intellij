9ca05de3e49db69f8f3629595cf4ee5cbecde61c
Firoz <shaik.2firoz@gmail.com>
2022-01-17 23:02:29 +0530

    This is For PR

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,5 +2,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev Class");
         System.out.println("New Statement");
+        System.out.println("PR Request");
     }
 }
--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -1,14 +1,6 @@
 public class DevTestClass {
     public static void main(String[] args) {
         System.out.println("Test Class");
-        //Test Class Comments
-        //Test Changes
-        //Test Changes -2
-
         System.out.println("feature1 - Changes from DevTest Class");
-        
-        // Forked Changes here
-
-        // New Comment Added
     }
 }
